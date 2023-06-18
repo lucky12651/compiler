2ebe2bad5bd4a8e0798af759e299027252236f19
Vaibhav Srivastava <vkluckymgs@gmail.com>
2023-06-18 21:12:06 +0530

    update

--- script5.js
+++ script5.js
@@ -20,39 +20,8 @@ function clearCode() {
     document.getElementById('codeTextarea').value = '';
 }
 
-function runCode() {
-    const code = document.getElementById('codeTextarea').value;
-    const input = document.getElementById('myTextarea').value;
 
-    const clientId = "fa607d20f8cfce0bb2b3ee95d869ee0e";
-    const clientSecret = "f778a799947f4ae19032dc7243003c7ee88a072b79443890110f56527ac23";
-    const language = "python";
-    const versionIndex = "3";
-
-    const data = {
-        script: code,
-        language: language,
-        versionIndex: versionIndex,
-        stdin: input,
-        clientId: clientId,
-        clientSecret: clientSecret
-    };
-
-    $.ajax({
-        url: "https://api.jdoodle.com/v1/execute",
-        type: "POST",
-        data: JSON.stringify(data),
-        contentType: "application/json",
-        dataType: "json",
-        success: function (result) {
-            const output = result.output;
-            document.getElementById('outputTextarea').value = output;
-        },
-        error: function (error) {
-            console.log(error);
-        }
-    });
-}
+  
 
 function uploadFile() {
    var input = document.createElement('input');
