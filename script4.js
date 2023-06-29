995d6f8d32ce00f670f98164f6c8624ef0b4bbd1
Vaibhav Srivastava <vkluckymgs@gmail.com>
2023-06-29 08:56:52 +0530

    bhot kuch add kiya hu

---
+++ script4.js
@@ -0,0 +1,24 @@
+window.addEventListener('load', function() {
+  var screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+  var isMobileDevice = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+
+  if (screenWidth < 960 || isMobileDevice) {
+    window.location.href = 'restricted.html';
+  }
+});
+function theme() {
+  const body = document.body;
+  const themeIcon = document.getElementById('theme-icon');
+
+  body.classList.toggle('dark-mode');
+
+  if (body.classList.contains('dark-mode')) {
+    // Switched to dark mode
+    themeIcon.src = 'sun1.gif';
+    themeIcon.title = 'Switch to light mode';
+  } else {
+    // Switched to light mode
+    themeIcon.src = 'moon.png';
+    themeIcon.title = 'Switch to dark mode';
+  }
+}
