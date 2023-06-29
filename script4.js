56c7ed23644539651d2050e334213085b6dd0585
Vaibhav Srivastava <vkluckymgs@gmail.com>
2023-06-29 08:55:51 +0530

    kuch bhi kaam ka nhi tha

--- script4.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('load', function() {
-  var screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-  var isMobileDevice = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-
-  if (screenWidth < 960 || isMobileDevice) {
-    window.location.href = 'restricted.html';
-  }
-});
