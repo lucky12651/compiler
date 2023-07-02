12cee35ea8de90f544a42ac402e3b11341d0d80b
Vaibhav Srivastava <vkluckymgs@gmail.com>
2023-07-02 09:35:30 +0530

    ..............added

---
+++ fire.js
@@ -0,0 +1,119 @@
+
+// Create a script element for SweetAlert library
+var sweetAlertScript = document.createElement('script');
+sweetAlertScript.src = 'https://cdn.jsdelivr.net/npm/sweetalert2@11.0.19/dist/sweetalert2.min.js';
+document.head.appendChild(sweetAlertScript);
+
+// Create a link element for SweetAlert CSS file
+var sweetAlertStylesheet = document.createElement('link');
+sweetAlertStylesheet.rel = 'stylesheet';
+sweetAlertStylesheet.href = 'https://cdn.jsdelivr.net/npm/sweetalert2@11.0.19/dist/sweetalert2.min.css';
+document.head.appendChild(sweetAlertStylesheet);
+
+// Rest of your code in fire.js
+// ...
+
+// Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyD29HcmBr1uCZCswiIUoJuo9XVJ3lVRxFs",
+    authDomain: "login-dedaa.firebaseapp.com",
+    databaseURL: "YOUR_DATABASE_URL",
+    projectId: "login-dedaa",
+    storageBucket: "login-dedaa.appspot.com",
+    messagingSenderId: "804295345267",
+    appId: "1:804295345267:web:d6b9a884763ddaa54dd606",
+    measurementId: "G-7J4798GMRC"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Get elements
+var loginForm = document.getElementById("loginForm");
+var signupForm = document.getElementById("signupForm");
+var errorMsg = document.getElementById("errorMsg");
+
+// Show login form by default
+loginForm.style.display = "block";
+signupForm.style.display = "none";
+
+// Handle navigation between login and signup forms
+document.getElementById("login").addEventListener("click", function(event) {
+    event.preventDefault();
+    loginForm.style.display = "block";
+    signupForm.style.display = "none";
+    errorMsg.textContent = "";
+    document.getElementById("login").classList.add("active");
+    document.getElementById("signup").classList.remove("active");
+});
+
+document.getElementById("signup").addEventListener("click", function(event) {
+    event.preventDefault();
+    loginForm.style.display = "none";
+    signupForm.style.display = "block";
+    errorMsg.textContent = "";
+    document.getElementById("login").classList.remove("active");
+    document.getElementById("signup").classList.add("active");
+});
+
+// Add login event
+loginForm.addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent form from submitting
+
+    var email = loginForm.email.value;
+    var password = loginForm.password.value;
+
+    // Sign in with email and password
+    firebase.auth().signInWithEmailAndPassword(email, password)
+        .then(function(user) {
+            // Login successful, redirect to index.html
+            Swal.fire({
+  icon: 'success',
+  title: 'Logged in Sucessfully',
+  text: 'press "ok" to continue.',
+  confirmButtonColor: '#3085d6',
+  confirmButtonText: 'OK',
+}).then((result) => {
+  if (result.isConfirmed) {
+    window.location.href = "index.html";
+  }
+});
+        })
+        .catch(function(error) {
+            // Handle errors here
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            errorMsg.textContent = "Error: " + errorCode + " - " + errorMessage;
+        });
+});
+
+// Add signup event
+signupForm.addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent form from submitting
+
+    var email = signupForm.email.value;
+    var password = signupForm.password.value;
+
+    // Create user with email and password
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+        .then(function(user) {
+            // Signup successful, redirect to index.html
+            Swal.fire({
+  icon: 'success',
+  title: 'Signed Up Succesfully',
+  text: 'press "ok" to continue.',
+  confirmButtonColor: '#3085d6',
+  confirmButtonText: 'OK',
+}).then((result) => {
+  if (result.isConfirmed) {
+    window.location.href = "index.html";
+  }
+});
+        })
+        .catch(function(error) {
+            // Handle errors here
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            errorMsg.textContent = "Error: " + errorCode + " - " + errorMessage;
+        });
+});
+
---
+++ script.js
@@ -0,0 +1,100 @@
+function showConsoleMessagesAndFloat() {
+    // Get the console viewer toggle and console viewer elements
+    var consoleViewerToggle = document.getElementById('console-viewer-toggle');
+    var consoleViewer = document.getElementById('console-viewer');
+  
+    // Set the initial position
+    var posX = 0;
+    var posY = 0;
+  
+    // Function to handle the start of dragging
+    function dragStart(event) {
+      posX = event.clientX - consoleViewer.offsetLeft;
+      posY = event.clientY - consoleViewer.offsetTop;
+      document.addEventListener('mousemove', dragMove);
+      document.addEventListener('mouseup', dragEnd);
+    }
+  
+    // Function to handle the dragging
+    function dragMove(event) {
+      consoleViewer.style.left = (event.clientX - posX) + 'px';
+      consoleViewer.style.top = (event.clientY - posY) + 'px';
+    }
+  
+    // Function to handle the end of dragging
+    function dragEnd() {
+      document.removeEventListener('mousemove', dragMove);
+      document.removeEventListener('mouseup', dragEnd);
+    }
+  
+    // Add event listener to the console viewer for dragging
+    consoleViewer.addEventListener('mousedown', dragStart);
+  
+    // Function to toggle the console viewer visibility
+    function toggleConsoleViewer() {
+      if (consoleViewer.style.display === 'none') {
+        consoleViewer.style.display = 'block';
+      } else {
+        consoleViewer.style.display = 'none';
+      }
+    }
+  
+    // Override the console.log function to capture messages
+    console.log = function(message) {
+      // Create a new paragraph element for each console message
+      var messageElement = document.createElement('p');
+      messageElement.textContent = message;
+  
+      // Append the message to the console output div
+      consoleViewer.querySelector('#console-output').appendChild(messageElement);
+    };
+  
+    // Attach the toggleConsoleViewer function to the toggle button's click event
+    consoleViewerToggle.addEventListener('click', toggleConsoleViewer);
+  }
+  var consoleViewerToggle = document.getElementById('console-viewer-toggle');
+  var consoleViewer = document.getElementById('console-viewer');
+
+  // Set the initial visibility of the console viewer
+  consoleViewer.style.display = 'none';
+
+  // Function to toggle the console viewer visibility
+  function toggleConsoleViewer() {
+    if (consoleViewer.style.display === 'none') {
+      consoleViewer.style.display = 'block';
+    } else {
+      consoleViewer.style.display = 'none';
+    }
+  }
+
+  // Attach the toggleConsoleViewer function to the toggle button's click event
+  consoleViewerToggle.addEventListener('click', toggleConsoleViewer);
+
+  // Override the console.log function to capture messages
+  console.log = function(message) {
+    var consoleOutput = document.getElementById('console-output');
+    var messageElement = document.createElement('p');
+    messageElement.textContent = message;
+    consoleOutput.appendChild(messageElement);
+  };  
+console.log("hi from 8bit")
+ const states = {
+    idle: '<i class="fas fa-play"></i>',
+    sending: '<img aria-hidden="true" width="24" height="24" src="https://assets.codepen.io/2585/ring-resize.svg" alt="Executing" />',
+    done: 'Done !',
+  }
+  
+  demo.onclick = () => {
+    setState('sending')
+    setTimeout(() => setState('done'), 3000)
+    setTimeout(() => setState('idle'), 5000)
+  }
+  
+  function setState(state) {
+    if (!document.startViewTransition)
+      demo.innerHTML = states[state]
+    else
+      document.startViewTransition(() => 
+        demo.innerHTML = states[state])
+  }  
+
